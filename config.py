22831f04826a40e9e971f052369d2ffd5a715638
Vagradez <vagradez@vistomail.com>
2017-04-25 21:12:20 +0300

    Update config.py
    set version to 0.0.1

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import landerdb
 
 relay = 0
 brokers = [{"ip":"zcoin.zapto.org", "port":6565}]
-version = "0.2.1"
+version = "0.0.1"
 host = "0.0.0.0"
 port = 6565
 nodes = landerdb.Connect("nodes.db")
